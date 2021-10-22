5541e9f1fda51df8c0e412e968a010c39c66df87
James Williamson <jwilliamson@gmail.com>
2021-10-22 13:56:33 +0100

    changed the text

--- hello_world_api.py
+++ hello_world_api.py
@@ -3,7 +3,7 @@ import json
 
 class HelloWorldResource:
     def on_get(self, req, resp):
-        result = {"result": "Hello Word!"}
+        result = {"result": "Hello Word! 2"}
         resp.text = json.dumps(result)
         resp.status = falcon.HTTP_OK
         resp.content_type = falcon.MEDIA_JSON
